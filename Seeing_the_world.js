08f0c3a13990018419322db81e1a88c02286c714
KomalBabar14 <komalbabar2009@gmail.com>
2024-03-20 10:56:50 +0500

    Created by Komal

---
+++ Seeing_the_world.js
@@ -0,0 +1,8 @@
+"use strict";
+let places = ["London", "Paris", "NewYork", "Bali", "Maldives"];
+console.log(places);
+var P = places.sort();
+console.log(P);
+console.log(places);
+var S = places.reverse();
+console.log(S);
---
+++ Seeing_the_world.ts
@@ -0,0 +1,7 @@
+let places= ["London","Paris","NewYork","Bali","Maldives"]
+console.log(places)// Original Array
+var P = places.sort()
+console.log(P)// Sorted Array
+console.log(places) //original array
+var S = places.reverse()
+console.log(S)// Reverse Arraymit
